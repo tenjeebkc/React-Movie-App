36ebd3a95c293dd8cb9296b4023e5589d1e4fea1
Tenjeeb K. C. <tenjeeb15@gmail.com>
2026-01-23 21:52:56 +0545

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,16 @@ import './App.css'
 
 function App() {
 const [query, setQuery] = useState("");
+const [movies, setMovies] = useState([])
 
-function handleSearch(){
-  console.log(query)
+async function handleSearch(){
+  if(!query) return;
+
+  const response = await fetch(
+     `https://www.omdbapi.com/?s=${query}&apikey=c443b2c5`
+  );
+  const data = await response.json();
+  setMovies(data.Search || []);
 }
 
   return (
@@ -18,7 +25,13 @@ function handleSearch(){
       value={query}
       onChange={(e)=> setQuery(e.target.value)} />
 
-      <button onClick={handleSearch} >Search</button>
+      <button onClick={handleSearch}>Search</button>
+
+      <div>
+        {movies.map((movie) => (
+          <p key={movie.imdbID}>{movie.Title}</p>
+        ))}
+      </div>
     </div>
     </>
   )
