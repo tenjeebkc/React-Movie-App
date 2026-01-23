bccfc1bd4616533700fa176e75bcc8aa1eef4574
Tenjeeb K. C. <tenjeeb15@gmail.com>
2026-01-23 19:41:00 +0545

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,23 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+const [query, setQuery] = useState("");
 
+function handleSearch(){
+  console.log(query)
+}
 
   return (
     <>
     <div>
       <h1>Movie App 🎬</h1>
+
+      <input type="text"
+      placeholder='Search Movie...'
+      value={query}
+      onChange={(e)=> setQuery(e.target.value)} />
+
+      <button onClick={handleSearch} >Search</button>
     </div>
     </>
   )
