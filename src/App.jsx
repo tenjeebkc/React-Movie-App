62e093998ea832428c957a306294e274b2577184
Tenjeeb K. C. <tenjeeb15@gmail.com>
2026-03-08 14:56:51 +0545

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,94 +1,17 @@
-import { useState } from 'react'
 import './App.css'
-import { Route, Routes, Link } from 'react-router-dom';
-import MovieDetails from './components/MovieDetails';
+import { Route, Routes } from 'react-router-dom';
+import MovieDetails from './pages/MovieDetails';
+import  Home  from './pages/Home';
 
 function App() {
-  const [query, setQuery] = useState("");
-  const [movies, setMovies] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
-
-  async function handleSearch() {
-    if (!query) return;
-
-    // Start loading & reset old data
-    setLoading(true);
-    setError("");
-    setMovies([]);
-
-    try {
-
-      const response = await fetch(
-        `https://www.omdbapi.com/?s=${query}&apikey=c443b2c5`
-      );
-      const data = await response.json();
-
-      // Handle movie not found
-      if (data.Response === "False") {
-        setError(data.Error);
-      }
-      else {
-        setMovies(data.Search);
-      }
-    } catch (err) {
-      setError("Something went wrong");
-    } finally {
-      setLoading(false);
-    }
-
-  }
-
   return (
     // <Routes> = Here are all the pages of my app
     // <Route path = "/" element = {...} /> = When url is /, Show this UI
     <Routes>
-      <Route
-      path='/'
-      element = {
-
-        <div>
-        <div className="heading">
-
-        <h1>Movie App🎬 - Made for you.</h1>
-        <h3>Search your favourite movies here.</h3>
-
-        <input type="text"
-          placeholder='Search Movie...'
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-          onKeyDown={(e) => e.key === "Enter" && handleSearch()} />
-
-        <button onClick={handleSearch}>Search</button>
-        {loading && <p>Loading...</p>}
-        {error && <p style={{color: "red"}}>{error}</p>}
-          </div>
-
-        <div className="movies">
-          {movies.map((movie) => (
-            <Link
-            to={`/movie/${movie.imdbID}`}
-            key={movie.imdbID}
-            style={{textDecoration:"none", color:"inherit"}}
-            >
-            <div className="movie-card" key={movie.imdbID}>
-              <img
-                src={movie.Poster !== "N/A" ? movie.Poster : ""}
-                alt={movie.Title}
-                />
-              <h3>{movie.Title}</h3>
-              <p>{movie.Year}</p>
-            </div>
-          </Link>
-          ))}
-        </div>
-      </div>
-    }/>
-
-    {/* Movie Details Path */}
-    <Route path='/movie/:id' element = {<MovieDetails/>}/>
+      <Route path='/' element={<Home />} />
+      <Route path='/movie/:id' element={<MovieDetails />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,14 +1,13 @@
-import React from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
-import { Navigate } from 'react-router-dom';
-
 
 function MovieDetails() {
     const { id } = useParams();  // get movie id from URL
+    const navigate = useNavigate();
+
     const [movie, setMovie] = useState(null);
     const [loading, setLoading] = useState(true);
-    const navigate = useNavigate();
+    const [error, setError] = useState("");
 
     useEffect(() => {
         async function fetchMovie() {
@@ -22,7 +21,6 @@ function MovieDetails() {
 
     if (loading) return <p>Loading...</p>
 
-
     return (
         <>
             {/* If you want to button to go to the specific router
@@ -30,7 +28,7 @@ function MovieDetails() {
             <div className='movie-details'>
             <button className='back-btn' onClick={() => navigate(-1)}>Back</button>
                 <h2>{movie.Title}</h2>
-                <img src={movie.Poster} alt="movie.Title" />
+                <img src={movie.Poster} alt={movie.Title} />
                 <p><strong>Year: </strong>{movie.Year}</p>
                 <p><strong>Genre: </strong>{movie.Genre}</p>
                 <p><strong>Plot: </strong>{movie.Plot}</p>
